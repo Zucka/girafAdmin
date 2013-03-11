82a3f5fc506d0136e2d4baa6b1b1f6c286f280a5
Tommy Knudsen <zfire911@gmail.com>
2013-03-11 11:33:06 +0100

    Added GPL v3 header

--- source/desktop/login.php
+++ source/desktop/login.php
@@ -1,3 +1,20 @@
+<!--
+This file is part of GIRAF.
+
+GIRAF is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+GIRAF is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GIRAF.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
 <?php
 	session_start();
 	require_once('db/db.php');
