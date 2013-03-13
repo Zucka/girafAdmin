84eb5c6837ebab8739a23fc7ca4cbc8d21adc6d3
Fruitdrops <fruitdrops13@hotmail.com>
2013-03-13 09:56:57 +0100

    headInclude.php and style.css for Menubar

---
+++ source/desktop/include/headInclude.php
@@ -0,0 +1,11 @@
+<?php
+	//Denne fil inkludere alle de væsentlige sider og scripts der hører til i Head
+?>
+	<html lang="en">
+	<meta charset="utf-8">
+	
+	<link href="assets/css/bootstrap.css" rel="stylesheet">
+	<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="assets/css/style.css" rel="stylesheet">
+	
+	<link rel="shortcut icon" href="../assets/ico/favicon.png">
