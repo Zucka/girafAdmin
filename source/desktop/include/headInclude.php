b10fe7be98d735552c6a68127cc1465b7bfb8486
Fruitdrops <fruitdrops13@hotmail.com>
2013-03-13 13:03:24 +0100

    Working on the Menu Design

--- source/desktop/include/headInclude.php
+++ source/desktop/include/headInclude.php
@@ -4,8 +4,16 @@
 	<html lang="en">
 	<meta charset="utf-8">
 	
+	<!-- Style Sheets -->
 	<link href="assets/css/bootstrap.css" rel="stylesheet">
 	<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
 	<link href="assets/css/style.css" rel="stylesheet">
+	
+	<!-- JavaScript -->
+	<script src="assets/js/bootstrap.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/main.js"></script>
+	<script src="assets/js/menu.js"></script>
 	
 	<link rel="shortcut icon" href="../assets/ico/favicon.png">
