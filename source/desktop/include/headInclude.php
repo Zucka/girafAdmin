57bfc5689937563ef2283a5558fd6cf98cec31e5
Fruitdrops <fruitdrops13@hotmail.com>
2013-03-13 14:00:04 +0100

    renamed menu to navigation

--- source/desktop/include/headInclude.php
+++ source/desktop/include/headInclude.php
@@ -14,6 +14,6 @@
 	<script src="assets/js/bootstrap.min.js"></script>
 	<script src="assets/js/jquery.min.js"></script>
 	<script src="assets/js/main.js"></script>
-	<script src="assets/js/menu.js"></script>
+	<script src="assets/js/navigation.js"></script>
 	
 	<link rel="shortcut icon" href="../assets/ico/favicon.png">
