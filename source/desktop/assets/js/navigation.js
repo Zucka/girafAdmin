e7c6acfddc9a62c327754c9e28f368dd47038b1d
Fruitdrops <fruitdrops13@hotmail.com>
2013-03-14 11:37:22 +0100

    Navigation - Functionality - Added description

--- source/desktop/assets/js/navigation.js
+++ source/desktop/assets/js/navigation.js
@@ -19,6 +19,13 @@ function resizeMe(){
 }
 
 window.onhashchange = function(){
+	/* This is the site-switch, it takes care of switching the content from the menu.
+	 * Beware of the syntax of links when you add them. Every new hashlink has to be of the form: "#link"/"extraInformation=1&extraInformation=2"
+	 * We use the "/" to seperate the link from the information. This also means that the "/"-char can not be used in any other relation.
+	 * 
+	 * If you want to add sites to the switch. First add them to the /index.php file, then add their hashLink here in the switch case.
+	 */
+
     var hashInfo = location.hash;    
 	var hashArray = hashInfo.split("/");
 	var destination = hashArray[0];
