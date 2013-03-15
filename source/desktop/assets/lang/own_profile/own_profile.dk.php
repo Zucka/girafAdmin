ebddf58d39bc4bd1bdd7bd0df118bb8f7f97e03e
Johan Sørensen <johan.soerensen6@gmail.com>
2013-03-15 14:36:44 +0100

    added lang support own profile

---
+++ source/desktop/assets/lang/own_profile/own_profile.dk.php
@@ -0,0 +1,31 @@
+<?php
+//DANISH LANGUAGE FILE, STRINGS SHOULD BE LOCATED IN $PROFILE_STRINGS["nameOfString"] = "STRING";
+
+		$PROFILE_STRINGS = array();
+
+		$PROFILE_STRINGS["headerTitle"] = "Egen Profil &middot; GIRAF";
+
+		$PROFILE_STRINGS["breadCrump"] = "Egen Profil";
+		$PROFILE_STRINGS["btnEdit"] = " Ret";
+
+
+		$PROFILE_STRINGS["h_personalInfo"] = "Personlig Information:";
+			$PROFILE_STRINGS["name"] = "Navn :";
+			$PROFILE_STRINGS["phoneNr"] = "Tlf :";
+			$PROFILE_STRINGS["mobilePhoneNr"] = "Mob :";
+			$PROFILE_STRINGS["address"] = "Adresse :";
+			$PROFILE_STRINGS["department"] = "Afdeling :";
+
+		$PROFILE_STRINGS["h_attachedChildren"] = "Tilknyttede Børn:";
+			$PROFILE_STRINGS["tblName"] = "Navn";
+			$PROFILE_STRINGS["tbParents"] = "Forældre";
+			$PROFILE_STRINGS["tblEditChild"] = "Ret barn profil";
+			$PROFILE_STRINGS["tblEditProfile"] = " Ret profil";
+
+		$PROFILE_STRINGS["pictureEdit"] = " Ændre";
+		$PROFILE_STRINGS["qrGenerateNew"] = " Generer ny";
+		$PROFILE_STRINGS["qrPrint"] = " Print";
+
+
+
+?>
---
+++ source/desktop/assets/lang/own_profile/own_profile.en.php
@@ -0,0 +1,31 @@
+<?php
+//ENGLISH LANGUAGE FILE, STRINGS SHOULD BE LOCATED IN $PROFILE_STRINGS["nameOfString"] = "STRING";
+
+		$PROFILE_STRINGS = array();
+
+		$PROFILE_STRINGS["headerTitle"] = "Profile Page &middot; GIRAF";
+
+		$PROFILE_STRINGS["breadCrump"] = "Profile Page";
+		$PROFILE_STRINGS["btnEdit"] = " Edit";
+
+
+		$PROFILE_STRINGS["h_personalInfo"] = "Personal Information:";
+			$PROFILE_STRINGS["name"] = "Name :";
+			$PROFILE_STRINGS["phoneNr"] = "Phone :";
+			$PROFILE_STRINGS["mobilePhoneNr"] = "Mobile :";
+			$PROFILE_STRINGS["address"] = "Address :";
+			$PROFILE_STRINGS["department"] = "Department :";
+
+		$PROFILE_STRINGS["h_attachedChildren"] = "Attached Children:";
+			$PROFILE_STRINGS["tblName"] = "Name";
+			$PROFILE_STRINGS["tbParents"] = "Parents";
+			$PROFILE_STRINGS["tblEditChild"] = "Edit Child";
+			$PROFILE_STRINGS["tblEditProfile"] = " Edit";
+
+		$PROFILE_STRINGS["pictureEdit"] = " Edit";
+		$PROFILE_STRINGS["qrGenerateNew"] = " Generate new";
+		$PROFILE_STRINGS["qrPrint"] = " Print";
+
+
+
+?>
