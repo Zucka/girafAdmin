e95f6f4a7efe554fda8355b4367c487ff51d4639
Johan Sørensen <johan.soerensen6@gmail.com>
2013-04-27 14:34:56 +0200

    Made own-profile work under all modes; accessible with: /action=department & /action=child

--- source/desktop/assets/lang/own_profile/own_profile.dk.php
+++ source/desktop/assets/lang/own_profile/own_profile.dk.php
@@ -19,7 +19,7 @@
 		$PROFILE_STRINGS["h_attachedChildren"] = "Tilknyttede Børn:";
 			$PROFILE_STRINGS["tblName"] = "Navn";
 			$PROFILE_STRINGS["tbParents"] = "Forældre";
-			$PROFILE_STRINGS["tblEditChild"] = "Ret barn profil";
+			$PROFILE_STRINGS["tblEditChild"] = "Ret barns profil";
 			$PROFILE_STRINGS["tblEditProfile"] = " Ret profil";
 
 		$PROFILE_STRINGS["pictureEdit"] = " Ændre";
@@ -30,6 +30,35 @@
 		$PROFILE_STRINGS["changePasswordSubmit"] = "Skift";
 		$PROFILE_STRINGS["qrGenerateNewAndPrint"] = "Generer ny QR og print";
 
+//DANISH LANGUAGE FILE, STRINGS SHOULD BE LOCATED IN $DEPARTMENT_STRINGS["nameOfString"] = "STRING";
 
+		$DEPARTMENT_STRINGS = array();
+
+		$DEPARTMENT_STRINGS["headerTitle"] = "Egen Profil &middot; GIRAF";
+
+		$DEPARTMENT_STRINGS["breadCrump"] = "Egen Profil";
+		$DEPARTMENT_STRINGS["btnEdit"] = " Ret";
+
+
+		$DEPARTMENT_STRINGS["h_personalInfo"] = "Personlig Information:";
+			$DEPARTMENT_STRINGS["name"] = "Navn :";
+			$DEPARTMENT_STRINGSDEPARTMENT_STRINGS["phoneNr"] = "Tlf :";
+			$DEPARTMENT_STRINGS["mobilePhoneNr"] = "Mob :";
+			$DEPARTMENT_STRINGS["address"] = "Adresse :";
+			$DEPARTMENT_STRINGS["department"] = "Afdeling :";
+
+		$DEPARTMENT_STRINGS["h_attachedChildren"] = "Tilknyttede Børn:";
+			$DEPARTMENT_STRINGS["tblName"] = "Navn";
+			$DEPARTMENT_STRINGS["tbParents"] = "Forældre";
+			$DEPARTMENT_STRINGS["tblEditChild"] = "Ret barn profil";
+			$DEPARTMENT_STRINGS["tblEditProfile"] = " Ret profil";
+
+		$DEPARTMENT_STRINGS["pictureEdit"] = " Ændre";
+		$DEPARTMENT_STRINGS["security"] = "Sikkerhed";
+		$DEPARTMENT_STRINGS["changePassword"] = "Skift kodeord";
+		$DEPARTMENT_STRINGS["placeholderPassword"] = "Kodeord";
+		$DEPARTMENT_STRINGS["placeholderRepeatPassword"] = "Kodeord Igen";
+		$DEPARTMENT_STRINGS["changePasswordSubmit"] = "Skift";
+		$DEPARTMENT_STRINGS["qrGenerateNewAndPrint"] = "Generer ny QR og print";
 
 ?>
--- source/desktop/sites/own_profile.php
+++ source/desktop/sites/own_profile.php
@@ -40,9 +40,15 @@ along with GIRAF.  If not, see <http://www.gnu.org/licenses/>.
 	{
 		$row = $result->fetch_assoc();
 	}
-echo '
+	if (isset($_GET["action"])) {$action = $_GET["action"];} else {$action = '';}
+	$title = getTitleFromAction($action);
+	$content = getContentFromAction($action);
 
-	<title>'.$PROFILE_STRINGS["headerTitle"].'</title>
+
+echo '
+<html lang="en">
+<head>
+	'.$title.'
 
 	<!-- JavaScript -->
 	<script src="assets/js/profileEdit.js"></script>
@@ -52,8 +58,53 @@ echo '
 	<link rel="stylesheet" type="text/css" href="include/jquery.imgareaselect-0.9.10/css/imgareaselect-default.css" />
 	<script type="text/javascript" src="include/jquery.imgareaselect-0.9.10/scripts/jquery.imgareaselect.pack.js"></script>
 
+</head>
+
+<body>
 	<div class="container-fluid">
-		<div class="breadcrump">'.$PROFILE_STRINGS["breadCrump"].'</div>
+		'.$content.'
+	</div>
+</body>
+<iframe name="print_frame" id="print_frame" width="0" height="0" frameborder="0" src="about:blank"></iframe>
+</html>
+';
+
+function getTitleFromAction($action)
+{
+	switch ($action) {
+		case '':
+		case 'pedagogue':
+			return '<title>'.$PROFILE_STRINGS["headerTitle"].'</title>';
+		case 'child':
+			return '<title>'.$PROFILE_STRINGS["headerTitle"].'</title>';
+		case 'department':
+			return '<title>'.$DEPARTMENT_STRINGS["headerTitle"].'</title>';
+		default:
+			return '<title>'.$PROFILE_STRINGS["headerTitle"].'</title>';
+	}
+}
+
+function getContentFromAction($action)
+{
+	switch ($action) {
+		case '':
+		case 'pedagogue':
+			return pedagogue();
+		case 'child':
+			return child();
+		case 'department':
+			return department();
+		default:
+			return pedagogue();
+	}
+}
+
+
+function pedagogue()
+{
+	global $PROFILE_STRINGS;
+	$content = '
+	<div class="breadcrump">'.$PROFILE_STRINGS["breadCrump"].'</div>
 		<div class="row">
 			<div class="span6">
 				<div class="container-fluid">
@@ -136,27 +187,178 @@ echo '
 				</div>
 			</div>
 		</div>
-	</div>
-
-	<iframe name="print_frame" id="print_frame" width="0" height="0" frameborder="0" src="about:blank"></iframe>
-';
-
-?>
+	';
+	return $content;
+}
 
 
+function child()
+{
+	global $PROFILE_STRINGS;
+	$content = '
+	<div class="breadcrump">'.$PROFILE_STRINGS["breadCrump"].'</div>
+		<div class="row">
+			<div class="span6">
+				<div class="container-fluid">
+					<h3>'.$PROFILE_STRINGS["h_personalInfo"].'</h3>
+					<table class="table table-profile">
+						<tr>
+							<td>'.$PROFILE_STRINGS["name"].'</td>
+							<td>Jens Lauritsen</td>
+							<td><button class="btn btn-mini buttonEdit" type="button"><i class="icon-wrench"></i>'.$PROFILE_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>'.$PROFILE_STRINGS["mother"].'</td>
+							<td>29 69 59 49</td>
+							<td><button class="btn btn-mini buttonEdit" type="button"><i class="icon-wrench"></i>'.$PROFILE_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>'.$PROFILE_STRINGS["father"].'</td>
+							<td>99 69 59 49</td>
+							<td><button class="btn btn-mini buttonEdit" type="button"><i class="icon-wrench"></i>'.$PROFILE_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>'.$PROFILE_STRINGS["address"].'</td>
+							<td>Bredgade 1, 9000 Aalborg</td>
+							<td><button class="btn btn-mini buttonEdit" type="button"><i class="icon-wrench"></i>'.$PROFILE_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>'.$PROFILE_STRINGS["department"].'</td>
+							<td>Bjælken</td>
+							<td><button class="btn btn-mini buttonEdit" type="button"><i class="icon-wrench"></i>'.$PROFILE_STRINGS["btnEdit"].'</button></td>
+						</tr>
+					</table>
+					<h3>'.$PROFILE_STRINGS["tbParents"].'</h3>
+					<table class="table table-striped">
+						<tr>
+							<th>'.$PROFILE_STRINGS["tblName"].'</th>
+							<th>'.$PROFILE_STRINGS["tblEditChild"].'</th>
+						</tr>
+						<tr>
+							<td>Jens Hansen</td>
+							<td><button class="btn btn-mini" type="button"><i class="icon-wrench"></i>'.$PROFILE_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>Pia Larsen</td>
+							<td><button class="btn btn-mini" type="button"><i class="icon-wrench"></i>'.$PROFILE_STRINGS["btnEdit"].'</button></td>
+						</tr>
+					</table>
+					<h3>'.$PROFILE_STRINGS["h_pedagogues"].'</h3>
+					<table class="table table-striped">
+						<tr>
+							<th>'.$PROFILE_STRINGS["tblName"].'</th>
+							<th>'.$PROFILE_STRINGS["tblEditRelation"].'</th>
+						</tr>
+						<tr>
+							<td>Jens Hansen</td>
+							<td><button class="btn btn-mini" type="button"><i class="icon-wrench"></i>'.$PROFILE_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>Pia Larsen</td>
+							<td><button class="btn btn-mini" type="button"><i class="icon-wrench"></i>'.$PROFILE_STRINGS["btnEdit"].'</button></td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<div class="span6">
+				<div>
+					<img class="profile_picture" src="../assets/img/girafAdminLogo-01.svg">
+					<button class="btn profile-btn" type="button" onclick="changeProfilePicturePopup()"><i class="icon-wrench"></i>'.$PROFILE_STRINGS["pictureEdit"].'</button>
+				</div>
+				<div class="profile-security">
+					<p class="lead text-center" id="security">'.$PROFILE_STRINGS["security"].'</p>
+					<div class="profile-qr">
+						<label>'.$PROFILE_STRINGS["changePassword"].'</label>
+						<form>
+							<input type="text" placeholder="'.$PROFILE_STRINGS["placeholderPassword"].'"><br>
+							<input type="text" placeholder="'.$PROFILE_STRINGS["placeholderRepeatPassword"].'"><br>
+							<input class="btn btn-primary" type="submit" value="'.$PROFILE_STRINGS["changePasswordSubmit"].'">
+						</form>
+						</br>
+						<button class="btn profile-btn" id="btn-gen" type="button"><i class="icon-wrench"></i>'.$PROFILE_STRINGS["qrGenerateNewAndPrint"].'</button>
+					</div>
+					<div class="profile-qr-status"> </div>
+				</div>
+			</div>
+		</div>
+	';
+	return $content;
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function department()
+{
+	global $DEPARTMENT_STRINGS;
+	$content = '
+	<div class="breadcrump">'.$DEPARTMENT_STRINGS["breadCrump"].'</div>
+		<div class="row">
+			<div class="span6">
+				<div class="container-fluid">
+					<h3>'.$DEPARTMENT_STRINGS["h_personalInfo"].'</h3>
+					<table class="table table-profile">
+						<tr>
+							<td>'.$DEPARTMENT_STRINGS["name"].'</td>
+							<td>Birken</td>
+							<td><button class="btn btn-mini buttonEdit" type="button"><i class="icon-wrench"></i>'.$DEPARTMENT_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>'.$DEPARTMENT_STRINGS["phoneNr"].'</td>
+							<td>29 69 59 70</td>
+							<td><button class="btn btn-mini buttonEdit" type="button"><i class="icon-wrench"></i>'.$DEPARTMENT_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>'.$DEPARTMENT_STRINGS["mobilePhoneNr"].'</td>
+							<td>99 69 59 80</td>
+							<td><button class="btn btn-mini buttonEdit" type="button"><i class="icon-wrench"></i>'.$DEPARTMENT_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>'.$DEPARTMENT_STRINGS["address"].'</td>
+							<td>Birkegade 1, 9000 Aalborg</td>
+							<td><button class="btn btn-mini buttonEdit" type="button"><i class="icon-wrench"></i>'.$DEPARTMENT_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>'.$DEPARTMENT_STRINGS["dpHead"].'</td>
+							<td>Hans Jensen</td>
+							<td><button class="btn btn-mini buttonEdit" type="button"><i class="icon-wrench"></i>'.$DEPARTMENT_STRINGS["btnEdit"].'</button></td>
+						</tr>
+					</table>
+					<h3>'.$DEPARTMENT_STRINGS["h_pedagogues"].'</h3>
+					<table class="table table-striped">
+						<tr>
+							<th>'.$DEPARTMENT_STRINGS["tblName"].'</th>
+							<th>'.$DEPARTMENT_STRINGS["tblAttachedChildren"].'</th>
+							<th>'.$DEPARTMENT_STRINGS["tblEditPedagogue"].'</th>
+						</tr>
+						<tr>
+							<td>Jens Hansen</td>
+							<td>Mai Petersen; Hans Jensen</td>
+							<td><button class="btn btn-mini" type="button"><i class="icon-wrench"></i>'.$DEPARTMENT_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>Hans Jensen</td>
+							<td>Anne Larsen; Peter Svendsen</td>
+							<td><button class="btn btn-mini" type="button"><i class="icon-wrench"></i>'.$DEPARTMENT_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>Pia Larsen</td>
+							<td>Gerta Larsen; Ken Larsen</td>
+							<td><button class="btn btn-mini" type="button"><i class="icon-wrench"></i>'.$DEPARTMENT_STRINGS["btnEdit"].'</button></td>
+						</tr>
+						<tr>
+							<td>Jens Hansen</td>
+							<td>Mai Petersen; Hans Jensen</td>
+							<td><button class="btn btn-mini" type="button"><i class="icon-wrench"></i>'.$DEPARTMENT_STRINGS["btnEdit"].'</button></td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<div class="span6">
+				<div>
+					<img class="profile_picture" src="../assets/img/randomHouse.svg">
+					<button class="btn profile-btn" type="button" onclick="changeProfilePicturePopup()"><i class="icon-wrench"></i>'.$DEPARTMENT_STRINGS["pictureEdit"].'</button>
+				</div>
+			</div>
+		</div>
+	';
+	return $content;
+}
