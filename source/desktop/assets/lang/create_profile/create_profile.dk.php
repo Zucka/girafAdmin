d8e6abb4a48edf24e3a267b9aa36368cbdbaa293
Jens Mikkel Lauridsen <jlauri10@student.aau.dk>
2013-05-07 13:02:52 +0200

    Create Pofile 1.0 ready, .js .php launge sup.

--- source/desktop/assets/lang/create_profile/create_profile.dk.php
+++ source/desktop/assets/lang/create_profile/create_profile.dk.php
@@ -1,15 +1,20 @@
 <?php
-//DANISH LANGUAGE FILE, STRINGS SHOULD BE LOCATED IN $LOGIN_STRINGS["nameOfString"]
+//DANISH LANGUAGE FILE, STRINGS SHOULD BE LOCATED IN $CREATEPROFILE_STRINGS["nameOfString"]
 
-		$LOGIN_STRINGS = array();
+		$CREATEPROFILE_STRINGS = array();
 
-		$LOGIN_STRINGS["headerTitle"] = "Log ind &middot; GIRAF";
-
-		$LOGIN_STRINGS["formUsername"] = "Brugernavn";
-		$LOGIN_STRINGS["formPassword"] = "Kodeord";
-		$LOGIN_STRINGS["formRememberMe"] = "Husk mig";
-		$LOGIN_STRINGS["formSignIn"] = "Log ind";
-
-		$LOGIN_STRINGS["errorUsernameNotFound"] = "Brugernavnet blev ikke fundet";
-		$LOGIN_STRINGS["errorWrongPassword"] = "Forkert kodeord";
+		$CREATEPROFILE_STRINGS["headerTitle"] = "Opret Profil i &middot; GIRAF";
+		$CREATEPROFILE_STRINGS["headline"] = "Opret Ny Profil";
+		$CREATEPROFILE_STRINGS["child"] = "Barn";
+		$CREATEPROFILE_STRINGS["parant"] = "Forældre";
+		$CREATEPROFILE_STRINGS["guardian"] = "Pædagog";
+		$CREATEPROFILE_STRINGS["name"] = "Navn";
+		$CREATEPROFILE_STRINGS["address"] = "Addresse";
+		$CREATEPROFILE_STRINGS["city"] = "By";
+		$CREATEPROFILE_STRINGS["zipcode"] = "Post Nummer";
+		$CREATEPROFILE_STRINGS["phone"] = "Telefon";
+		$CREATEPROFILE_STRINGS["mobile"] = "Mobil";
+		$CREATEPROFILE_STRINGS["email"] = "Email";
+		$CREATEPROFILE_STRINGS["createprofile"] = "Opret Profil";
+		$CREATEPROFILE_STRINGS["dataRequired"] = "* Makerede felter er nødvendige<br> for oprettelse af profil";
 ?>
