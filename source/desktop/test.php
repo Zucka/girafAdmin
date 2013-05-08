4dda68cf74c846e170fd9c5539849f3a3aa12e73
Tommy Knudsen <zfire911@gmail.com>
2013-05-08 10:11:56 +0200

    fixed json

--- source/desktop/test.php
+++ source/desktop/test.php
@@ -1,12 +1,12 @@
 <?
 	$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 	socket_connect($socket, '172.25.26.181',2468);
-	$buffer = "{
-		'auth': {
-		'username':'John',
-		'password':'123456'
-		}
-	}";
+	$buffer = '{
+	    "auth": {
+	        "username": "John",
+	        "password": "123456"
+	    }
+	}';
 	socket_write($socket, $buffer, strlen($buffer));
 	sleep(1);
 	$buf = '';
