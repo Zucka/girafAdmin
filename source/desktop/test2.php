3528613e97f361d4a2dbff547b17a003bbc6fb92
Tommy Knudsen <zfire911@gmail.com>
2013-05-12 12:58:37 +0200

    Test2, for viewing json response from profile list view

---
+++ source/desktop/test2.php
@@ -0,0 +1,22 @@
+<?
+	$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+	socket_connect($socket, '172.25.26.181',2468);
+	$buffer = '{
+		"action": "read",
+	    "auth": {
+	        "username": "John",
+	        "password": "123456"
+	    },
+	    "data": {
+	    	"type":"profile",
+	    	"view":"list",
+	    	"ids":null
+	    }
+	}';
+	socket_write($socket, $buffer, strlen($buffer));
+	sleep(1);
+	$buf = '';
+	if (false !== ($bytes = socket_recv($socket, $buf, 2048, MSG_WAITALL))) {
+		echo $buf;
+	}
+?>
