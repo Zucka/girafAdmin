4a2a2dcbfa5211073973a4d768d1db68cdd575fb
Tommy Knudsen <zfire911@gmail.com>
2013-05-12 23:51:27 +0200

    testing

--- source/desktop/test2.php
+++ source/desktop/test2.php
@@ -2,16 +2,23 @@
 	$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 	socket_connect($socket, '130.225.196.27',2468);
 	$buffer = '{
-		"action": "read",
-	    "auth": {
-	        "username": "John",
-	        "password": "123456"
-	    },
-	    "data": {
-	    	"type":"profile",
-	    	"view":"details",
-	    	"ids":[1]
-	    }
+		"auth": {
+			"username": "John",
+			"password": "123456"
+		},
+		"action": "update",
+		"data": {
+			"type": "user",
+			"values": [
+				{
+					"id": 1,
+					"value": {
+						"certificate": "fhsduifhiuewuifn49t32ntr2ufni23d",
+						"id":1
+					}
+				}
+			]
+		}
 	}';
 	socket_write($socket, $buffer, strlen($buffer));
 	sleep(1);
