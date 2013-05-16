f8c053c906473b2b363ab6dd436263111073cda4
Tommy Knudsen <zfire911@gmail.com>
2013-05-16 17:11:46 +0200

    added more json error messages

--- source/desktop/test2.php
+++ source/desktop/test2.php
@@ -27,6 +27,8 @@
     JSON_ERROR_DEPTH => 'The maximum stack depth has been exceeded',
     JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
     JSON_ERROR_SYNTAX => 'Syntax error',
+    JSON_ERROR_STATE_MISMATCH => 'Invalid or malformed JSON',
+    JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
 	);
  	echo 'Last error : ', $json_errors[json_last_error()], PHP_EOL, PHP_EOL;
 	print_r($array);
