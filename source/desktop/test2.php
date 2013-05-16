5e9ec4de477c62031f0f1a527c4468f8ef0bc767
Tommy Knudsen <zfire911@gmail.com>
2013-05-16 17:08:47 +0200

    json debugging

--- source/desktop/test2.php
+++ source/desktop/test2.php
@@ -22,5 +22,12 @@
 	}
 	$array = json_decode($final,true);
 	print_r($final);
+	$json_errors = array(
+    JSON_ERROR_NONE => 'No error has occurred',
+    JSON_ERROR_DEPTH => 'The maximum stack depth has been exceeded',
+    JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
+    JSON_ERROR_SYNTAX => 'Syntax error',
+	);
+ 	echo 'Last error : ', $json_errors[json_last_error()], PHP_EOL, PHP_EOL;
 	print_r($array);
 ?>
