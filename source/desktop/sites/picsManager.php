4fe20c0e6504bc07711d74ece15f7d0226ead53b
Tommy Knudsen <zfire911@gmail.com>
2013-06-28 08:09:38 +0200

    temporary fix for language

--- source/desktop/sites/picsManager.php
+++ source/desktop/sites/picsManager.php
@@ -1,7 +1,7 @@
 <div id="picsManagerNavi">
 	<h1>Pics Manager</h1>
-	<a href="/#makePic"><button>Make</button></a><br>
-	<a href=""><button>Add</button></a><br>
+	<a href="/#makePic"><button>Create</button></a><br>
+	<a href=""><button>Assign</button></a><br>
 	<a href=""><button>Remove</button></a><br>
 	<a href=""><button>Edit</button></a><br>
 	<a href=""><button>Delete</button></a><br>
